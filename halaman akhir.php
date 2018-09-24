4e2bb98df0a13057b63a8220f0ef767d9abda679
DidaPradana <42725223+didapradana@users.noreply.github.com>
2018-09-24 10:54:48 +0700

    Update halaman akhir.php

--- halaman akhir.php
+++ halaman akhir.php
@@ -1,1 +1,26 @@
-halaman akhir bisa diperlukan
+<?php 
+session_start();
+$data = $_SESSION['user'];
+?>
+
+<table width="900px" align="center" border="2">
+	<tr align="left">
+		<th>Nama</th>
+		<th>Hobi</th>
+		<th>Gambar</th>
+	</tr>
+	<tr valign="top">
+		<td><?php echo $data['nama']; ?></td>
+		<td>
+			<?php
+				foreach ($data['hobi'] as $hobbies => $value) {
+			 		echo $value . '<br>';
+			 	} 	
+			?>
+		</td>
+		<td>
+			<img src="<?php echo $data["gambar"]; ?>" alt="">
+		</td>
+    </tr>
+    <a href="formawal.html">Hapus</a>
+</table>
